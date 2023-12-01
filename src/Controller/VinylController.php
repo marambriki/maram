713d94cf648c283435285ada02d8d1031f8f7370
Maram Briki <brikimaram308@gmail.com>
2023-12-01 19:05:58 +0100

    Querying the Database

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,13 +2,21 @@
 
 namespace App\Controller;
 
-use function Symfony\Component\String\u;
+use App\Repository\VinylMixRepository;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
+    public function __construct(
+        private bool $isDebug,
+        private VinylMixRepository $mixRepository
+    )
+    {}
+
     #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
@@ -20,6 +28,7 @@ class VinylController extends AbstractController
             ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
+
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks,
@@ -27,11 +36,15 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        $mixes = $mixRepository->findAll();
+
         return $this->render('vinyl/browse.html.twig', [
-            'genre' => $genre
+            'genre' => $genre,
+            'mixes' => $mixes,
         ]);
     }
 }
